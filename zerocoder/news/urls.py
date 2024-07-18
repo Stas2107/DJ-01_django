a1614865a7fd6b891d4380e9f261afd39990ca89
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-07-18 13:56:54 +0400

    1

--- zerocoder/news/urls.py
+++ zerocoder/news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='home'),
+    path('', views.home, name='news_home'),
     # path('new', views.new, name='page2'),
     # path('data', views.data),
     # path('test', views.test),
